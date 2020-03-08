e9df3cba628e1047b353c4cfb588288866a113ee
LukeDelaCruz <lukedc28@gmail.com>
2020-03-08 11:15:09 -0600

    fixing broken build, cherrypick revert DBManger Trips, and TripSearchActivity commits

--- application/app/src/main/java/com/example/buber/Views/Activities/MapActivity.java
+++ application/app/src/main/java/com/example/buber/Views/Activities/MapActivity.java
@@ -80,16 +80,6 @@ public class MapActivity extends AppCompatActivity implements Observer, OnMapRea
         showSideBar = false;
 
         App.getModel().addObserver(this);
-
-        riderRequestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), TripSearchActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
     }
 
     public void initializeMap(){
