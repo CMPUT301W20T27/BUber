fd192017322e46fc40c394daa42cbc8f0b3a4d9e
Michael Sandare <sandare@ualberta.ca>
2020-03-12 16:08:41 -0600

    changes to EditAccountTest - nearly complete, issue with sidebar clicker

--- application/app/src/androidTest/java/com/example/buber/EditAccountTest.java
+++ application/app/src/androidTest/java/com/example/buber/EditAccountTest.java
@@ -1,11 +1,13 @@
 package com.example.buber;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.buber.Controllers.EventCompletionListener;
 import com.example.buber.Model.Account;
 import com.example.buber.Model.Rider;
 import com.example.buber.Model.User;
@@ -19,10 +21,14 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.HashMap;
+
+import static java.lang.System.err;
 import static junit.framework.TestCase.assertTrue;
 
 public class EditAccountTest {
     private Solo solo;
+
 
     //Signs out previous user and "signs in" a mockUser for testing
     public EditAccountTest(){
@@ -31,24 +37,64 @@ public class EditAccountTest {
         //Create mockAccount and mockUser -> setSessionUser(mockUser)
         // Note: this logs a mockUser into the app
         Account mockAccount = new Account("Test", "User", "testUser@test.test", "0001112222");
-        User mockUser = new Rider("mockUser", mockAccount);
+        //User mockUser = new Rider("mockUser", mockAccount);
+        Rider rider = new Rider("mockUser", mockAccount);
+        rider.setRiderLoggedOn(true);
+        User mockUser = rider;
         App.getModel().setSessionUser(mockUser);
+        EventCompletionListener listener = (resultData, err) -> {
+        };
+        App.getAuthDBManager().signIn("testUser@test.test","password", listener);
+
     }
 
     @Rule
-    public ActivityTestRule<EditAccountActivity> rule = new ActivityTestRule<>(EditAccountActivity.class, true, true);
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    //@Rule
+    //public ActivityTestRule<EditAccountActivity> rule = new ActivityTestRule<>(EditAccountActivity.class, true, true);
+
 
     //Note: app requires an account to be logged in to interact with map screen activities
     @Before
     public void setUp() throws Exception{
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
-    }
+        solo.enterText((EditText) solo.getView(R.id.loginEmailEditText), "testUser@test.test");
+        solo.enterText((EditText) solo.getView(R.id.loginPasswordEditText), "password");
+        solo.clickOnButton("Login as Rider");
+        solo.waitForText("You are NOW logged in.", 1, 2000);
+        solo.waitForActivity(MapActivity.class);
 
+
+        //solo.clickOnView(solo.getView(R.id.settings_button));
+        //solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule2.getActivity());
+        //solo.clickOnButton(0);
+        solo.sleep(2000);
+        solo.assertCurrentActivity("Wrong Activity", MapActivity.class);
+
+        //solo.clickOnButton(R.id.settings_button);
+        //solo.clickOnButton("|||");
+        solo.clickOnScreen(50,50);
+        //solo.setNavigationDrawer(solo.OPENED);
+        //solo.clickOnButton("Account");
+    }
+    /*
+    @Before
+    public void setUp(){
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+    */
     //Tests a correct change to the User's EditAccount
     @Test
     public void testCorrectEditAccount(){
         //Assert that test in EditAccountActivity
         solo.assertCurrentActivity("Wrong Activity", EditAccountActivity.class);
+
+        //clear all fields
+        solo.clearEditText((EditText) solo.getView(R.id.createAccountUsername));
+        solo.clearEditText((EditText) solo.getView(R.id.createAccountFirstName));
+        solo.clearEditText((EditText) solo.getView(R.id.createAccountLastName));
+        solo.clearEditText((EditText) solo.getView(R.id.createAccountPhoneNumber));
 
         //Change EditAccountActivity info
         solo.enterText((EditText) solo.getView(R.id.createAccountUsername), "newTesterChange");
@@ -62,7 +108,9 @@ public class EditAccountTest {
         //solo.clickOnView(solo.getView(R.id.buttonEditAccountSave));   //Note: this is just another way to click the same "Save Changes" button as above
 
         //Assert that user returned to MapActivity
-        assertTrue(solo.waitForActivity(MapActivity.class));
+        //solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        assertTrue(solo.waitForActivity(MainActivity.class));
     }
 
     //Tests a incorrect input in EditAccount Activity
