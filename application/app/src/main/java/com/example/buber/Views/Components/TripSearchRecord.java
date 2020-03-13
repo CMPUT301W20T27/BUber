3bc41d2c4c9aa005673bf49914748aa00ee54372
Michael Sandare <sandare@ualberta.ca>
2020-03-12 20:54:13 -0600

    Changed TripSearch Address from geolocation to address. Overhaul of colour scheme - clearer UI for trip search view and fragment

--- application/app/src/main/java/com/example/buber/Views/Components/TripSearchRecord.java
+++ application/app/src/main/java/com/example/buber/Views/Components/TripSearchRecord.java
@@ -10,8 +10,8 @@ import java.io.Serializable;
  */
 public class TripSearchRecord implements Serializable {
     private String riderName;
-    private String startLatitude;
-    private String startLongitude;
+    private String startAddress;
+    private String endAddress;
     private String endLatitude;
     private String endLongitude;
     private String estimatedCost;
@@ -19,28 +19,22 @@ public class TripSearchRecord implements Serializable {
 
     public TripSearchRecord(Trip t, UserLocation driverLocation){
         this.riderName = t.getRiderUserName();
-        this.startLatitude = ((Double) t.getStartUserLocation().getLatitude()).toString();
-        this.startLongitude = ((Double) t.getStartUserLocation().getLongitude()).toString();
-        this.endLatitude = ((Double) t.getEndUserLocation().getLatitude()).toString();
-        this.endLongitude = ((Double) t.getEndUserLocation().getLongitude()).toString();
+        this.startAddress = t.getStartUserLocation().getAddress();
+        this.endAddress = t.getEndUserLocation().getAddress();
         this.estimatedCost = ((Double) t.getFareOffering()).toString();
         this.distanceFromDriver = ((Double) t.getStartUserLocation().distanceTo(driverLocation))
                 .toString();
     }
 
     public String getRiderName(){return this.riderName;}
-    public String getStartLatitude(){return this.startLatitude;}
-    public String getStartLongitude(){return this.startLongitude;}
-    public String getEndLatitude(){return this.endLatitude;}
-    public String getEndLongitude(){return this.endLongitude;}
+    public String getStartAddress(){return this.startAddress;}
+    public String getEndAddress(){return this.endAddress;}
     public String getEstimatedCost(){return this.estimatedCost;}
     public String getDistanceFromDriver(){return this.distanceFromDriver;}
 
     public void setRiderName(String newRiderName){this.riderName = newRiderName;}
-    public void setStartLatitude(String newStartLat){this.startLatitude = newStartLat;}
-    public void setStartLongitude(String newStartLong){this.startLongitude = newStartLong;}
-    public void setEndLatitude(String newEndLat){this.endLatitude = newEndLat;}
-    public void setEndLongitude(String newEndLong){this.endLongitude = newEndLong;}
+    public void setStartAddress(String newStartLat){this.startAddress = newStartLat;}
+    public void setEndAddress(String newStartLong){this.endAddress = newStartLong;}
     public void setEstimatedCost(String newEstimatedCost){this.estimatedCost = newEstimatedCost;}
     public void setDistanceFromDriver(String newDistance){this.distanceFromDriver = newDistance;}
 }
